a66a7f7bc2e3baaa6814027ad5458491db2d6c30
Jacob Pattison <jpattison129@gmail.com>
2024-03-07 12:39:11 +0000

    Initial commit

---
+++ Assets/Scripts/CatalogueNavigation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatalogueNavigation : MonoBehaviour
+{
+    public TMPro.TMP_Text Title;
+    public TMPro.TMP_Text Information;
+
+    public Text[] InformationText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string title = InformationText[0].text.Split(',')[0];
+        string information = InformationText[0].text.Split(',')[0];
+        Title.text = title;
+        Information.text = information;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public TMPro.TMP_Text LeaderboardText;
+    public List<string> players = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
